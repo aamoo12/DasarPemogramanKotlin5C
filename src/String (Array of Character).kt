f93dd7586f971f0bcd306570f02a9fb1c4d0ddf7
SD-22 <irhamni168@gmail.com>
2024-09-03 13:45:05 +0700

    NEXT

---
+++ src/String (Array of Character).kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    val myName: String = "Depandi Enda"
+    for(chr in myName){
+        print(chr)
+    }
+    //
+    print('\n')
+    val s = "abc" + 1
+    println(s + "def")
+    //
+    var text = """
+for (c in "foo")
+print(c)
+"""
+    println(text)
+    //
+}
